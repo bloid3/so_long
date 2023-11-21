0a533ef376dfe8d575e960499c2006c699597095
Pablo Pereira <papereir@c1r1s2.42madrid.com>
2023-11-21 18:12:04 +0100

    2111

--- checkmap.c
+++ checkmap.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   checkmap.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/16 18:18:59 by papereir          #+#    #+#             */
-/*   Updated: 2023/11/21 17:47:17 by papereir         ###   ########.fr       */
+/*   Created: 2023/11/21 17:37:35 by papereir          #+#    #+#             */
+/*   Updated: 2023/11/21 17:40:47 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main(int argc, char **argv)
+t_map struct_init(t_map *map)
 {
-	t_map	map;
-
-	if (argc != 2)
-		return (0);
-	map = struct_init(&map);
-	map.s_xpm.mlx = mlx_init();
-	return (0);
+    map->height = 0;
+    map->cigs = 0;
+    map->player = 0;
+    map->exit = 0;
+    map->s_player.x = 0;
+    map->s_player.y = 0;
+    map->s_player.movements = 0;
+    return (*map);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:16:31 by papereir          #+#    #+#             */
-/*   Updated: 2023/11/16 18:35:40 by papereir         ###   ########.fr       */
+/*   Updated: 2023/11/21 18:11:49 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,43 +21,58 @@
 # include <math.h>
 # include "libft/libft.h"
 # include <stdarg.h>
+# define BUFFER_SIZE 42
+
+//GNL FUNCTIONS
+
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*get_next_line(int fd);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+
 
 typedef struct s_player
 {
-	int	x;
-	int	y;
-	int	movements;
+	int x;
+	int y;
+	int movements;
 }t_player;
-typedef struct s_xpm
+
+typedef struct s_xpm 
 {
-	void	*mlx;
-	void	*mlx_win;
-	void	*wall;
-	void	*door;
-	void	*light;
-	void	*p;
-	void	*floor;
-	void	*open_door;
+	void *mlx;
+	void *mlx_win;
+	void *wall;
+	void *door;
+	void *light;
+	void *p;
+	void *floor;
+	void *open_door;
 }t_xpm;
 
 typedef struct s_map
 {
-	char		**map;
-	char		**copy;
-	int			height;
-	int			width;
-	int			things;
-	int			player;
-	int			exit;
-	t_xpm		s_xpm;
-	t_player	s_player;		
-}t_map;
+	char **map;
+	char **copy;
+	int height;
+	int width;
+	int cigs;
+	int player;
+	int exit;
+	t_xpm s_xpm;
+	t_player s_player;
+	
+} t_map;
 
-typedef struct s_data {
+
+typedef struct	s_data { 
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
 }t_data;
+
+t_map struct_init(t_map *map);
+
 #endif
